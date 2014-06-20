9fb79d28336879a8b207ad6f33ab887619a492c0
Jan Vermeir <jvermeir@xebia.com>
2014-06-20 22:00:31 +0200

    FIxed error in class level comment of FixtureTemplate and updated blog text.

--- src/main/java/com/xebia/concordion/FixtureTemplate.java
+++ src/main/java/com/xebia/concordion/FixtureTemplate.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 
 /**
  * This template is used by <code>ConcordionRunner</code> to generate a Fixture class.
- * It's subclassed by <code>MyFixture</code>.
  */
 @RunWith(ConcordionRunner.class)
 public class FixtureTemplate extends MyFixture {
